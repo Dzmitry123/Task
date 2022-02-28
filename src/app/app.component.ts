98270d7848d142dce956e49f7a1cc82622416831
Dzmitry123 <d.popichev@gmail.com>
2022-02-28 18:45:06 +0300

    firstcell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import {Component, OnInit} from '@angular/core';
 export interface Player {
   name: string;
 }
+export interface Cell {
+  x: number,
+  y: number,
+  crossed: string
+}
 
 @Component({
   selector: 'app-root',
@@ -13,13 +18,20 @@ export class AppComponent implements OnInit {
 
   public players: Player[] = [];
   public namePlayer = '';
+  public cells: Cell[] = [];
+
 
   constructor() {
   }
 
   ngOnInit() {
 
+    for (let i=0; i<3; i++) {
+      let cell: Cell = {x: i, y: 0, crossed: 'empty'};
+      this.cells.push(cell)
+    }
   }
+
 
 
   addPlayer() {
