b65badbf2f57e9eb04d7f7de21f7c9a6dcfacf33
Dzmitry123 <d.popichev@gmail.com>
2022-02-28 15:28:26 +0300

    player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {delay, filter, map} from "rxjs";
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'task';
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = [];
+
+  todoPlayer = '';
+  loading = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {}
+
+
+  addPlayer() {
+    if (!this.todoPlayer.trim()) {
+      return;
+    }
+    const newTodo: Todo = {
+      title: this.todoPlayer,
+      completed: false
+    }
+   }
+
+
+
 }
