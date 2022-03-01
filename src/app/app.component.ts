9d3efcca83065c5e2cb4cd46b0313f6ccc768b19
Dzmitry123 <d.popichev@gmail.com>
2022-03-01 12:08:14 +0300

    withzerro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
   public players: Player[] = [];
   public namePlayer = '';
   public field: Cell[][] = [[], [], []];
+  public turn: number = 0;
 
 
   constructor() {
@@ -28,23 +29,32 @@ export class AppComponent implements OnInit {
   ngOnInit() {
 
     for (let i = 0; i < 3; i++) {
-      for (let j=0; j < 3; j++) {
+      for (let j = 0; j < 3; j++) {
         let cell: Cell = {x: i, y: j, status: 'empty'};
         this.field[i].push(cell)
       }
-     }
+    }
   }
 
 
   addPlayer() {
-    let player: Player = {name: this.namePlayer};
-    this.players.push(player);
-    this.namePlayer = '';
-    console.log(this.players.length)
+    if (this.namePlayer.trim()) {
+      let player: Player = {name: this.namePlayer};
+      this.players.push(player);
+      this.namePlayer = '';
+      console.log(this.players.length)
+    }
   }
 
-  crossed(cell: Cell) {
-    cell.status = 'crossed';
-
+  click(cell: Cell) {
+    if (cell.status == 'empty') {
+      if (this.turn % 2 == 0) {
+        cell.status = 'crossed'
+      } else {
+        cell.status = 'zero'
+      }
+      this.turn++;
+      console.log(this.turn)
+    }
   }
 }
